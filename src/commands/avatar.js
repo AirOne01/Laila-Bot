4271c86478ee1c9a210b477d41bb4c8f85f77881
TheYellowBush <popGthyrd@gmail.com>
2019-06-20 17:24:45 +0200

    Added avatar command

---
+++ src/commands/avatar.js
@@ -0,0 +1,49 @@
+// avatar.js
+// A simple command that send an embed message of your avatar or the avatar of someone else.
+
+const Discord = require('discord.js');
+
+const cfg = require('../../config/config.json');
+
+module.exports = function (msg, args, DiscordClient) {
+
+    if (!args[0] || msg.mentions.users.first().id === msg.author.id) {
+                                                // If no args are provided or the author asked for its own avatar
+        embed = new Discord.RichEmbed()
+            .setColor(cfg.color)
+            .setTitle('Your avatar :')
+            .setImage(msg.author.avatarURL);    // The bot simply send the message author's avatar
+
+        msg.channel.send(embed);
+
+    } else if (msg.mentions.users.first().id == DiscordClient.user.id) {
+                                                // If the message author asked for the bot avatar:
+        embed = new Discord.RichEmbed()
+            .setColor(cfg.color)
+            .setTitle('My avatar :')
+            .setImage(DiscordClient.user.avatarURL)
+                                                // Send the bot avatar
+        msg.channel.send(embed);
+
+    } else if (msg.mentions.users.first()) {
+
+        embed = new Discord.RichEmbed()
+            .setColor(cfg.color)
+            .setTitle(msg.mentions.users.first().username + '\'s avatar')
+            .setImage(msg.mentions.users.first().avatarURL)
+
+        msg.channel.send(embed);
+
+    } else {
+                                                // Else
+
+        embed = new Discord.RichEmbed()
+            .setColor(cfg.color)
+            .setTitle('Nope.')
+            .setDescription('Please mention someone to get his avatar.')
+
+        msg.channel.send(embed);
+
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
+// Hi, fellow traveller, and welcome to the
+//   _           _ _                                                        _
+//  | |         (_) |                                                      | |
+//  | |     __ _ _| | __ _    ___  ___  _   _ _ __ ___ ___     ___ ___   __| | ___
+//  | |    / _` | | |/ _` |  / __|/ _ \| | | | '__/ __/ _ \   / __/ _ \ / _` |/ _ \
+//  | |___| (_| | | | (_| |  \__ \ (_) | |_| | | | (_|  __/  | (_| (_) | (_| |  __/
+//  |______\__,_|_|_|\__,_|  |___/\___/ \__,_|_|  \___\___|   \___\___/ \__,_|\___|
+//
+// Made by IsekaiTensei
+// This code is provided under the GNU GPL License.
 
+const Discord = require('discord.js');
+
+const DiscordClient = new Discord.Client();
+
+const cfg = require('../config/config.json');
+const commandAvatar = require('./commands/avatar');
+
+const prefix = cfg.prefix;
+
+DiscordClient.on('ready', () => {
+
+    console.log('Client connected as ' + DiscordClient.user.tag)
+
+})
+
+DiscordClient.on('message', (msg) => {
+
+    if (!msg.author.bot) {
+
+        if (msg.content.startsWith(prefix)) {
+
+            var args = msg.content.slice(prefix.length).split(' ');
+            var command = args.shift().toLowerCase();
+
+            switch (command) {
+
+                case 'avatar':
+
+                    console.log(typeof(DiscordClient.user.id));
+
+                    commandAvatar(msg, args, DiscordClient);
+                    break;
+
+                
+
+            }
+
+        }
+
+    }
+
+})
+
+DiscordClient.login(cfg.discordToken);
